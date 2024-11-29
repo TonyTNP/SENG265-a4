7caa04660d3331ab0e3cc44b950876a3adce1ccf
Ashton Majachani <amajachani@uvicv.ca>
2024-11-29 11:28:32 -0800

    added lab11

---
+++ Lab11/lab-11/B/product_table_gui.py
@@ -0,0 +1,62 @@
+import sys
+
+from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import QApplication, QMainWindow, QVBoxLayout
+from PyQt6.QtWidgets import QPushButton, QTableView, QWidget
+
+from controller import Controller
+from product import Product
+from product_table_model import ProductTableModel
+
+class ProductTableGUI(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.controller = Controller()
+        self.setWindowTitle("Products")
+        self.resize(600, 400)
+
+        self.product_table = QTableView()
+
+        self.product_model = ProductTableModel(self.controller)
+        self.product_table.setModel(self.product_model)
+
+        refresh_button = QPushButton("Refresh")
+        refresh_button.clicked.connect(self.refresh_button_clicked)
+        clear_button = QPushButton("Clear")
+        clear_button.clicked.connect(self.clear_button_clicked)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.product_table)
+        layout.addWidget(refresh_button)
+        layout.addWidget(clear_button)
+
+        widget = QWidget()
+        widget.setLayout(layout)
+        self.setCentralWidget(widget)
+        self.refresh_button_clicked()
+
+
+    def refresh_button_clicked(self):
+        # TODO: After you complete the refresh_data() method in the table model 
+        # class, call this method, fix the product description's column width to
+        # better use the window size, and make the product table editable.
+
+
+
+
+    def clear_button_clicked(self):
+        # TODO: call the reset() method in the table model and disable edits in
+        #  the product table. (Do you understand why you should disable it?).
+
+
+
+
+def main():
+    app = QApplication(sys.argv)
+    window = ProductTableGUI()
+    window.show()
+    app.exec()
+
+if __name__ == '__main__':
+    main()
