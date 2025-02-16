928c5e8c004e819211f3c07bc2f0fd109a5f6afe
Ashton Majachani <amajachani@uvic.ca>
2025-02-15 20:44:40 -0800

    Cleaned up repo, removed the unnecessary files

--- a2/output.h
+++
@@ -1,8 +0,0 @@
-#ifndef _OUTPUT_H_
-#define _OUTPUT_H_
-
-#include "dyn_survey.h"
-
-void print_output(int config_bits[MAX_CONFIG_BITS], char questions[MAX_QUESTIONS][MAX_QUESTION_LEN], char likert_array[MAX_TYPES][MAX_TYPES_LEN], int reverse_array[MAX_QUESTIONS], int **frequencies, int num_respondents, int **scores_array);
-
-#endif
